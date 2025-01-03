4ba23d33b7fe1d479799e1f6ae203883802e2278
danyal-0-1 <dkhorami@asu.edu>
2025-01-03 12:57:35 -0700

    i2caddr.ino

--- Test Code/i2caddr.ino
+++ Test Code/i2caddr.ino
@@ -1,28 +1,44 @@
-#include "Wire.h"
+#include <Wire.h>
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
+  Wire.begin(18, 19);  // Use correct SDA and SCL pins for your ESP32-C3
+
+  Serial.println("\nI2C Scanner");
 }
 
 void loop() {
   byte error, address;
-  int nDevices = 0;
+  int nDevices;
 
-  delay(5000);
+  Serial.println("Scanning...");
 
-  Serial.println("Scanning for I2C devices ...");
-  for(address = 0x01; address < 0x7f; address++){
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
-    if (error == 0){
-      Serial.printf("I2C device found at address 0x%02X\n", address);
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.print(address, HEX);
+      Serial.println("  !");
       nDevices++;
-    } else if(error != 2){
-      Serial.printf("Error %d at address 0x%02X\n", error, address);
     }
+    else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+    }    
   }
-  if (nDevices == 0){
-    Serial.println("No I2C devices found");
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
   }
-}
+  else {
+    Serial.println("done\n");
+  }
+  delay(5000);  
